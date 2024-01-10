64c81100ab8c0c8add4d01656b7a1d419fa7b001
KonstantinBenenson <km.benenson@gmail.com>
2024-01-11 00:52:21 +0300

    Added axios fetching via Rapid API from Youtube API v3 (getting videos with the query parameter). Added styling for the video page and not it`s rendering videos on the page (for now - just passing the alias "VideoCard" instead of the video cards themself).

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,32 +1,62 @@
 import { useState, useEffect } from "react";
 import { Box, Stack, Typography } from "@mui/material";
-import Sidebar from "./Sidebar";
+import { Sidebar, Videos } from "./";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
 
-const Feed = () => (
+const Feed = () => {
+  const [selectedCategory, setSelectedCategory] = useState("New");
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
+      .then((data) => setVideos(data.items))
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, [selectedCategory]);
+
   // Implemented logic where if we`re viewing on a PC or a tablet (md+) all videos are going to go in rows (horizontally),
   // but when it`s on a mobile phone (sx) - it`s going to be shown vertically stacked (in a column)
-  <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
-    <Box
-      sx={{
-        height: {
-          sx: "auto",
-          md: "92vh",
-          borderRight: "1px solid #3d3d3d",
-          px: { sx: 0, md: 2 },
-        },
-      }}
-    >
-      <Sidebar />
-
-      <Typography
-        className="copyright"
-        variant="body2"
-        sx={{ mt: 5, color: "#fff" }}
+  return (
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+      <Box
+        sx={{
+          height: {
+            sx: "auto",
+            md: "92vh",
+            borderRight: "1px solid #3d3d3d",
+            px: { sx: 0, md: 2 },
+          },
+        }}
       >
-        Copyright 2024 Beyond Dreams.
-      </Typography>
-    </Box>
-  </Stack>
-);
+        <Sidebar
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+        />
+
+        <Typography
+          className="copyright"
+          variant="body2"
+          sx={{ mt: 5, color: "#fff" }}
+        >
+          Copyright 2024 Beyond Dreams.
+        </Typography>
+      </Box>
+
+      <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          mb={2}
+          sx={{ color: "white" }}
+        >
+          {selectedCategory} <span style={{ color: "#FC5103" }}>videos</span>
+        </Typography>
+
+        <Videos videos={videos} />
+      </Box>
+    </Stack>
+  );
+};
 
 export default Feed;
